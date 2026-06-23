0d95f2dfde6626676d238e86b7af85d0b93b83a2
fortuneugroupofficial-sys <fortuneugroupofficial@gmail.com>
2026-06-23 16:16:48 +0530

    Add Disclosure component with information paragraph

---
+++ frontend/src/pages/Disclosure.jsx
@@ -0,0 +1,8 @@
+export default function Disclosure() {
+  return (
+    <div>
+      <h1>Disclosure</h1>
+      <p>Fortune U Group may receive referral or affiliate commissions from certain products and services.</p>
+    </div>
+  );
+}
